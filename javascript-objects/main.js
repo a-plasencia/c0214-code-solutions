261a407f1e389e200c371aefb30e109ef29bc2bd
Alex Plasencia <aPlasencia895@hotmail.com>
2025-04-03 21:18:05 +0000

    Completed javascript-objects exercise.

--- javascript-objects/main.js
+++ javascript-objects/main.js
@@ -15,3 +15,24 @@ student.previousOccupation = 'Coding instructor';
 console.log('Value of likesInIrvine is: ', student.likesInIrvine);
 console.log('Value of previousOccupation is: ', student.previousOccupation);
 console.log('Value of entire object of student is: ', student);
+
+var vehicle = {
+  make: 'Hyundai',
+  model: 'Kona',
+  year: 2019,
+};
+
+vehicle['color'] = 'white';
+vehicle['isConvertible'] = false;
+console.log('Value of color is: ', vehicle.color);
+console.log('Value of isConvertible is: ', vehicle.isConvertible);
+console.log('Value of entire vehicle object is: ', vehicle);
+
+var pet = {
+  name: 'Kobe',
+  type: 'Dog',
+};
+
+delete pet.name;
+delete pet.type;
+console.log('Value of entire pet object is: ', pet);
