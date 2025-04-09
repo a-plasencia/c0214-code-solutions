edc99031297d26fc7fcfbf58d6fda9350f2f9c55
Alex Plasencia <aPlasencia895@hotmail.com>
2025-04-09 18:06:35 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,65 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4th, 2020',
+    orderNumber: '114-3941689-8772232',
+    orderDelivered: 'August 8th, 2020',
+    total: '$31.55',
+    shipTo: 'JS Masher',
+    item: [
+      {
+        title: 'JavaScript for impatient programmers',
+        price: '$31.55',
+        returnWindow: 'Return window closed on Sep 7, 2020',
+      },
+    ],
+  },
+  {
+    orderPlaced: 'July 19th, 2020',
+    orderNumber: '113-9984268-1280257',
+    orderDelivered: 'July 20th, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    item: [
+      {
+        title: 'The Timeless Way of Building',
+        price: '$44.33',
+        returnWindow: 'Return window closed on Aug 19, 2020',
+      },
+    ],
+  },
+  {
+    orderPlaced: 'July 4th, 2020',
+    orderNumber: '114-2875557-9059409',
+    orderDelivered: 'July 5th, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    item: [
+      {
+        title:
+          'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        price: '$94.95',
+        returnWindow: 'Return window closed on Aug 4th, 2020',
+      },
+    ],
+  },
+  {
+    orderPlaced: 'July 3rd, 2020',
+    orderNumber: '113-2883177-2648248',
+    orderDelivered: 'July 5th, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    item: [
+      {
+        title:
+          'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        price: '$94.95',
+        returnWindow: 'Return window closed on Aug 4th, 2020',
+      },
+      {
+        title: 'The Art of Sql',
+        price: '$34.99',
+        returnWindow: 'Return window closed on Aug 4th, 2020',
+      },
+    ],
+  },
+];
