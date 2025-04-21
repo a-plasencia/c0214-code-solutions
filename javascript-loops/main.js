ce61c4f0792aad7ebdf9ab3f25d89e4c8b16fdfe
Alex Plasencia <aPlasencia895@hotmail.com>
2025-04-21 21:08:18 +0000

    Began exercise finish tomorrow.

---
+++ javascript-loops/main.js
@@ -0,0 +1,15 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+
+console.log('Hello world');
+
+function getNumbersToTen() {
+  var numbers = [];
+  var currentNumber = 1;
+}
