10647eff0cccaef0f0cf9200c8832f4d8d52e50c
Alex Plasencia <aPlasencia895@hotmail.com>
2025-04-22 21:24:16 +0000

    Almost done with the loops exercise.

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -7,9 +7,40 @@
             getValues
  */
 
-console.log('Hello world');
-
 function getNumbersToTen() {
   var numbers = [];
   var currentNumber = 1;
+  while (currentNumber <= 10) {
+    numbers.push(currentNumber);
+    currentNumber += 1;
+  }
+  return numbers;
 }
+console.log('The value of getNumbersToTen is: ', getNumbersToTen());
+
+function getEvenNumbersToTwenty() {
+  var evenNumbers = [];
+  var currentNumber = 2;
+  while (currentNumber <= 20) {
+    evenNumbers.push(currentNumber);
+    currentNumber += 2;
+  }
+  return evenNumbers;
+}
+
+console.log('The value of getEvenNumbers is: ', getEvenNumbersToTwenty());
+
+function repeatWord(word, times) {
+  var count = 1;
+  var repeated = '';
+  while (count < times) {
+    repeated += word;
+    count += 1;
+  }
+  return repeated;
+}
+
+console.log(
+  'Calling the word JavaScript in repeatWord 30 times',
+  repeatWord('JavaScript', 30)
+);
