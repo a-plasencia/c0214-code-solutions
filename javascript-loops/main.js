b877c1be66d9a81671ebbe7c075c5589b3197650
Alex Plasencia <aPlasencia895@hotmail.com>
2025-04-23 19:29:36 +0000

    Completed javascript-loops exercise.

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -44,3 +44,49 @@ console.log(
   'Calling the word JavaScript in repeatWord 30 times',
   repeatWord('JavaScript', 30)
 );
+
+function logEachCharacter(string) {
+  for (var i = 0; i < string.length; i++) {
+    console.log('The value of string[i] is: ', string[i]);
+  }
+}
+
+logEachCharacter('JavaScript');
+
+function doubleAll(numbers) {
+  var doubled = [];
+  for (var i = 0; i < numbers.length; i++) {
+    var doubledNumber = numbers[i] * numbers[i];
+    doubled.push(doubledNumber);
+  }
+  return doubled;
+}
+
+var doubleArray = [1, 5, 8, 10];
+console.log('The value of doubleArrayTwo is: ', doubleAll(doubleArray));
+
+function getKeys(object) {
+  var keys = [];
+  for (var key in object) {
+    keys.push(key);
+  }
+  return keys;
+}
+
+var teams = {
+  football: 'raiders',
+  basketball: 'lakers',
+  baseball: 'dodgers',
+};
+
+console.log('value of getkeys: is: ', getKeys(teams));
+
+function getValues(object) {
+  var values = [];
+  for (var key in object) {
+    values.push(object[key]);
+  }
+  return values;
+}
+
+console.log('value of getValues is: ', getValues(teams));
